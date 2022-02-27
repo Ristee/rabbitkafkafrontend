abec2177e92c008f8f33819fa407d11d39b4a1af
ristee <rristee@gmail.com>
2022-02-27 23:21:59 +0500

    start add components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,80 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <header class="app-header">
+      <NavLinks/>
+    </header>
+    <div class="app-content">
+      <main class="container">
+        <router-view/>
+      </main>
+    </div>
+
+    <footer>
+      <div class="container">
+        <hr/>
+        <p>&copy; App. All rights reserved.</p>
+      </div>
+    </footer>
   </div>
 </template>
 
+<script>
+import NavLinks from "@/components/NavLinks";
+
+export default {
+  components: {
+    NavLinks
+  }
+}
+</script>
+
 <style lang="scss">
+body {
+  min-height: 85vh;
+  padding-top: 5rem;
+}
+
+html {
+  position: relative;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  min-height: 85vh;
+  display: flex;
+  flex-direction: column;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app-content {
+  flex: 1;
+  padding: 1rem 0;
 }
+
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  line-height: 60px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+
+body > .container {
+  padding: 60px 15px 0;
+}
+
+.footer > .container {
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+code {
+  font-size: 80%;
+}
+
 </style>
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow fixed-top">
+    <h5 class="my-0 mr-md-auto font-weight-normal">
+      <router-link :to="{name: 'home'}">App name</router-link>
+    </h5>
+    <router-link class="p-2 text-dark" :to="{name: 'about'}">About</router-link>
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="#">Sign In</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Sign up</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavLinks'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
